5b42760ec41b2b8735db91ca1f4a5a923bc2ab0f
Anton Tmur <tmur.a@numdes.com>
2022-09-14 13:55:29 +0400

    problem_049_group_anagrams added

---
+++ problem_049_group_anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        d = dict()
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            d[sorted_s] = d.get(sorted_s, []) + [s]
+
+        res = []
+        for key, val in d.items():
+            res.append(val)
+
+        return res
