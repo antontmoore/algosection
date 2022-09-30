b8effab5e1d69544489cb4aca1d051f5bd49e4ee
Anton Tmur <tmur.a@numdes.com>
2022-09-30 22:04:32 +0400

    problem_088_merge_sorted_array added

---
+++ problem_088_merge_sorted_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        i, j = m-1, n-1
+        for k in range(m+n-1, -1, -1):
+            if j < 0 or (i >= 0 and j >= 0 and nums1[i] > nums2[j]):
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+
+        a = 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    s.merge(nums1, m, nums2, n)
+    print(nums1)
