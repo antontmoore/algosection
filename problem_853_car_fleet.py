73a7825846a3bd7e9aae521a51f30fa4e6737cdf
Anton Tmur <tmur.a@numdes.com>
2025-06-20 20:45:47 +0100

    problem_853_car_fleet.py changed

--- problem_853_car_fleet.py
+++ problem_853_car_fleet.py
@@ -1,16 +1,22 @@
 from typing import List
 
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ps = sorted(list(zip(position, speed)), key=lambda x: x[0])
+        cars = sorted(zip(position, speed))
 
-        number_of_fleets = 1
-        last_car = ps.pop()
-        current_fleet_time_at_target = (target - last_car[0]) / last_car[1]
-        for car in ps[::-1]:
-            time_at_target = (target - car[0]) / car[1]
-            if time_at_target > current_fleet_time_at_target:
-                number_of_fleets += 1
-                current_fleet_time_at_target = time_at_target
+        times = [(target - p) / s for p, s in cars]
+        ans = 0
+        while len(times) > 1:
+            lead = times.pop()
+            if lead < times[-1]:
+                ans += 1
+            else:
+                times[-1] = lead
+        return ans + bool(times)
 
-        return number_of_fleets
+
+if __name__ == "__main__":
+    s = Solution()
+    res = s.carFleet(target=100, position=[0, 2, 4], speed=[4, 2, 1])
+    print(res)
