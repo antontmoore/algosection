998de4833602ba9b7b418a171dcb81e9e032f343
Anton Tmur <tmur.a@numdes.com>
2025-07-10 17:05:36 +0100

    problem_076_minimum_window_substring.py added

---
+++ problem_076_minimum_window_substring.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def idx(letter):
+            return ord(letter) - ord('A')
+
+        def is_substring(big_arr, small_arr):
+            for j in range(len(big_arr)):
+                if small_arr[j] > big_arr[j]:
+                    return False
+            return True
+
+        total_number_of_letters = idx('z') - idx('A')
+        t_arr = [0 for _ in range(total_number_of_letters)]
+        s_arr = [0 for _ in range(total_number_of_letters)]
+
+        for c in t:
+            t_arr[idx(c)] += 1
+
+        have, need = 0, len(t)
+        min_length = 2 * len(s)
+        best_window = ""
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            s_arr[idx(c)] += 1
+
+            if t_arr[idx(c)] == s_arr[idx(c)]:
+                have += 1
+
+            while have == need:
+                if r - l + 1 < min_length:
+                    min_length = r - l + 1
+                    best_window = s[l: r + 1]
+
+                s_arr[idx(s[l])] -= 1
+                if t_arr[idx(s[l])] > s_arr[idx(s[l])]:
+                    have -= 1
+                l += 1
+
+        return best_window
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    res = s.minWindow("ADOBECODEBANC", "ABC")
+    res = s.minWindow("aa", "aa")
+    print(res)
