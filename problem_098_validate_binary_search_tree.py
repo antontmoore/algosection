2906002311d919bf8cfad9730b8281f1c02949ce
Anton Tmur <tmur.a@numdes.com>
2025-07-13 21:14:34 +0100

    problem_098_validate_binary_search_tree.py added

---
+++ problem_098_validate_binary_search_tree.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def helper(node, low=-math.inf, high=math.inf):
+            if node is None:
+                return True
+
+            if node.val <= low or node.val >= high:
+                return False
+
+            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+
+        return helper(root)
