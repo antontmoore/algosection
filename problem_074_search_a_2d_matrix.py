d913e2f3512ff49b38e0dc04fd153dd8a49953ae
Anton Tmur <tmur.a@numdes.com>
2025-07-30 17:35:12 +0100

    problem_074_search_a_2d_matrix.py changed

--- problem_074_search_a_2d_matrix.py
+++ problem_074_search_a_2d_matrix.py
@@ -3,41 +3,33 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
 
-        n = len(matrix[0])
-        # looking for the row
-        top, bottom = 0, len(matrix)-1
+        def idx(r, c):
+            return r * n + c
 
-        while top <= bottom:
-            row = (top + bottom) // 2
+        def rc(idx):
+            return idx // n, idx % n
 
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bottom = row - 1
-            else:
-                break
-
-        # looking for the column
-        left, right = 0, len(matrix[0])-1
-
-        while right > left:
+        left, right = 0, m * n - 1
+        while left <= right:
             mid = (left + right) // 2
-
-            if matrix[row][mid] == target:
+            row, col = rc(mid)
+            if matrix[row][col] == target:
                 return True
-            if matrix[row][mid] > target:
-                right = mid - 1
-            else:
+            elif matrix[row][col] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
 
-        return matrix[row][left] == target
+        return False
 
 
 if __name__ == "__main__":
     # matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
     # target = 13
     matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+    matrix = [[1]]
     target = 8
     s = Solution()
     print(s.searchMatrix(matrix, target))
