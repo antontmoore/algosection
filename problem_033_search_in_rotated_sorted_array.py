2e862a849d0fa04faa5b598cb053e6e80657454d
Anton Tmur <tmur.a@numdes.com>
2025-07-30 22:31:24 +0100

    problem_033_search_in_rotated_sorted_array.py changed

--- problem_033_search_in_rotated_sorted_array.py
+++ problem_033_search_in_rotated_sorted_array.py
@@ -4,40 +4,34 @@ from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
-        def binary_search(arr, left, right, target):
-            while left < right:
-                mid = (left + right) // 2
-                if arr[mid] < target:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[mid] >= nums[left]:
+                # left part is sorted
+                if target < nums[mid] and target >= nums[left]:
+                    # target inside left part
+                    right = mid - 1
+                else:
+                    # let's look for it in the right part
+                    left = mid + 1
+            else:
+                # right part is sorted
+                if target > nums[mid] and target <= nums[right]:
+                    # target inside right part
                     left = mid + 1
                 else:
-                    right = mid
-
-            return left if target == arr[left] else -1
-
-        left, right = 0, len(nums)-1
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] < nums[right]:
-                # sorted in right part
-                if nums[mid] <= target <= nums[right]:
-                    return binary_search(nums, mid, right, target)
-                else:
-                    # but we need to go to the left
-                    right = mid
-
-            elif nums[left] <= nums[mid]:
-                # sorted in left part
-                if nums[left] <= target <= nums[mid]:
-                    return binary_search(nums, left, mid, target)
-                else:
-                    # but we need to go to the right
-                    left = mid+1
-
-        return left if target == nums[left] else -1
+                    # let's look for it in left part
+                    right = mid - 1
+        return -1
 
 
 if __name__ == "__main__":
     s = Solution()
-    nums = [5, 6, 7, 8, 9, 10, 1, 2, 3, 4]
-    ans = s.search(nums, 10)
+    # nums = [5, 6, 7, 8, 9, 10, 1, 2, 3, 4]
+    nums = [3, 1]
+    ans = s.search(nums, 1)
     print(ans)
