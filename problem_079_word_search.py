4b04c2fc269502792c6a67c07d798992575ea3ed
Anton Tmur <tmur.a@numdes.com>
2025-08-07 15:39:09 +0100

    problem_079_word_search.py changed

--- problem_079_word_search.py
+++ problem_079_word_search.py
@@ -5,42 +5,38 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
 
-        def backtrack(row, col, w, used):
-            if len(w) == 0:
+        def dfs(cell: tuple[int], idx: int):
+            i, j = cell
+            if board[i][j] != word[idx]:
+                return False
+            elif idx == len(word) - 1:
                 return True
+            else:
+                move_up, move_down, move_left, move_right = False, False, False, False
+                used.add(cell)
+                if i > 0 and (i - 1, j) not in used:
+                    move_up = dfs((i - 1, j), idx + 1)
 
-            flag = False
-            if row > 0 and board[row-1][col] == w[0] and (row-1, col) not in used:
-                used.add((row - 1, col))
-                flag |= backtrack(row-1, col, w[1:], used)
-                used.remove((row-1, col))
+                if i < m - 1 and (i + 1, j) not in used:
+                    move_down = dfs((i + 1, j), idx + 1)
 
-            if col > 0 and board[row][col-1] == w[0] and (row, col-1) not in used:
-                used.add((row, col - 1))
-                flag |= backtrack(row, col-1, w[1:], used)
-                used.remove((row, col - 1))
+                if j > 0 and (i, j - 1) not in used:
+                    move_left = dfs((i, j - 1), idx + 1)
 
-            if row < m-1 and board[row+1][col] == w[0] and (row+1, col) not in used:
-                used.add((row + 1, col))
-                flag |= backtrack(row+1, col, w[1:], used)
-                used.remove((row + 1, col))
+                if j < n - 1 and (i, j + 1) not in used:
+                    move_right = dfs((i, j + 1), idx + 1)
 
-            if col < n-1 and board[row][col+1] == w[0] and (row, col+1) not in used:
-                used.add((row, col + 1))
-                flag |= backtrack(row, col+1, w[1:], used)
-                used.remove((row, col + 1))
+                if cell in used:
+                    used.remove(cell)
 
-            return flag
+                return move_up or move_down or move_left or move_right
 
-
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    used = set()
-                    used.add((i, j))
-                    flag = backtrack(i, j, word[1:], used)
-                    if flag: return flag
-
+        for start_i in range(m):
+            for start_j in range(n):
+                used = set()
+                flag = dfs((start_i, start_j), 0)
+                if flag:
+                    return True
         return False
 
 
@@ -49,6 +45,6 @@ if __name__ == "__main__":
     # board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
     # word = "ABCB"
     board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-    word = "ABCCED"
+    word = "ABCB"
 
     print(s.exist(board, word))
